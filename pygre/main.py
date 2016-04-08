66437f5896ebed790ac01e7227ccf9cbec4b31a2
Francois Best <francoisbest@gmail.com>
2016-04-08 09:00:24 +0200

    Templates & application stub.

--- pygre/main.py
+++ pygre/main.py
@@ -1,7 +1,51 @@
 # -*- coding: utf-8 -*-
 
+import os
+import tornado.web
+import tornado.ioloop
+import tornado.options
+
+# ------------------------------------------------------------------------------
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('index.html')
+
+class RegisterHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('register.html')
+
+# ------------------------------------------------------------------------------
+
+class PyGreApplication(tornado.web.Application):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.port = kwargs.get('port', 8080)
+
+    def start(self):
+        try:
+            self.listen(self.port)
+            print('Listening on localhost:{}'.format(self.port))
+            tornado.ioloop.IOLoop.current().start()
+        except KeyboardInterrupt:
+            print('KeyboardInterrupt: Shutting down...')
+
+# ------------------------------------------------------------------------------
+
 def main():
-    print('Hello, PyGre.io !')
+    root_directory = os.path.join(os.path.dirname(__file__))
+    app = PyGreApplication(
+        handlers=[
+            # Handlers go here
+            (r'/',          IndexHandler),
+            (r'/register',  RegisterHandler),
+        ],
+        port=8080,
+        template_path=os.path.join(root_directory, 'templates'),
+        static_path=os.path.join(root_directory, 'static'),
+        debug=True,
+    )
+    app.start()
 
 # ------------------------------------------------------------------------------
 
